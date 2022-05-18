34758910906d22983d7a628b11e6d3fe4ae8b201
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-18 16:42:14 -0700

    home page btn

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/todo");
+  };
+  return (
+    <div className="container my-5">
+      <div className="w-25 mx-auto">
+        {" "}
+        <button
+          onClick={handleNavigate}
+          className="w-100 p-2 btn btn-outline-info"
+        >
+          MANAGE TODO
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
