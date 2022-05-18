ae7d4e144f03624e982e5fba218141ca798558d3
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-18 08:30:20 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Todo from "./components/Todo/Todo";
+import Login from "./components/Authentication/Login";
+import SignUp from "./components/Authentication/SignUp";
+import NavBar from "./components/Shared/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-primary">hi</h1>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/todo" element={<Todo></Todo>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Shared/NavBar.js
+++ src/components/Shared/NavBar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="px-5 bg-light d-flex justify-content-between align-items-center w-100 py-2">
+      <div className="w-25">
+        {" "}
+        <h4 className="brand my-auto">MY TODO LIST</h4>
+      </div>
+      <div className="w-75 d-flex items-center justify-content-end align-items-center">
+        <Link to={"/home"} className="mx-3 nav-items">
+          HOME
+        </Link>
+        <Link to={"/login"} className="mx-3 nav-items">
+          LOGIN
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
