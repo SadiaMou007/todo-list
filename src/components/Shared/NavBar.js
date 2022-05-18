c358433e79c7120f20e49a1557eb1905f4e838ea
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-18 12:14:28 -0700

    togo form added

--- src/components/Shared/NavBar.js
+++ src/components/Shared/NavBar.js
@@ -3,7 +3,6 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../../firebase.init";
-import Loading from "./Loading";
 import "./Navbar.css";
 
 const NavBar = () => {
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const nameRef = useRef("");
+  const detailRef = useRef("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const detail = detailRef.current.value;
+
+    console.log(name, detail);
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto my-5 border p-3 rounded">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div class="my-auto col-lg-3">
+              <input
+                placeholder="Name"
+                type="text"
+                name="name"
+                class="form-control border-info"
+                id="name"
+                ref={nameRef}
+                required
+              />
+            </div>
+            <div class="my-2 col-lg-6">
+              <textarea
+                placeholder="Task Details"
+                ref={detailRef}
+                required
+                class="form-control border-info"
+                name="description"
+                id="des"
+                rows="3"
+              ></textarea>
+            </div>
+            <div className="col-lg-3 my-auto">
+              <button type="submit" class="btn btn-outline-success ">
+                ADD TASK
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
